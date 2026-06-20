8caedf7e872b52cd371095892067c1fd35484a8c
Emmanuval  George <emmanuvalgeorge16.com>
2026-06-20 15:28:14 +0530

    Initial version-2

--- Main.py
+++ Main.py
@@ -1,2 +1,13 @@
 import os
-import
+import sys
+
+from PIL import Image # Pillow library for image processing
+from reportlab.lib.pagesizes import letter # ReportLab library for PDF generation
+from reportlab.pdfgen import canvas # ReportLab library for PDF generation
+
+SPPORTED_IMAGE_FORMATS = ('.jpg', '.jpeg', '.png', '.bmp',)#NO gif or webp because of transparency issues
+
+PAGE_WIDTH, PAGE_HEIGHT = letter #612, 792
+PAGE_MARGIN = 36 # better layout and less ugly
+
+# thats all for the base
